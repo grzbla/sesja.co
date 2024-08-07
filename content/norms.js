44528547263534f4cdfbf17629b15ae3e24fdce9
grzbla <neoniusz+behold@gmail.com>
2024-08-07 22:18:50 +0200

    rules

---
+++ content/norms.js
@@ -0,0 +1,66 @@
+const norm = {
+    rules:
+    {
+        0: 'Can survive in equitable utopian society.',
+        1: ['Able to survive in generous and forgiving environment.',
+            'Capable of operating on binary level: yes/no, right/wrong, left/right'],
+        2: ['Will prosper with lifetime worth of inheritance.',
+            'Capable of producing words to back up own stance.'],
+        3: ['Could achieve a livable standard in free and fair environment.',
+            'Can form indeptendent opinions.'],
+        4: ['Will prosper in limited or closely controlled economy.',
+            'Can predict and avoid mistakes.'],
+        5: ['Capable of surviving in resource scarce environment.',
+            'Can formulate complex plans.'],
+        6: ['Able to thrive in a cut-throat dog eat dog world.',
+            'Does always extra.']
+    },
+    skills:
+    {
+        "Luck":
+        {
+            type: 'upgrades or something',
+            "Beginner's Luck": ['Get +3N bonus.', 'Boost rolls before level 3.'],
+            "Intermediate's Luck": ['Get +5N bonus.', 'Can boost rolls until level 5.'],
+            "Pro Luck": ['Get +1D4 Luck Die.']
+        },
+        "":
+        {
+
+        }
+    },
+    epochs:
+    {
+        archaic:
+        {
+            human:
+            {
+                hunting: 5,
+                gathering: 5
+            },
+
+        },
+        ancient:
+        {
+            human:
+            {
+                master:
+                {
+
+                },
+                slave:
+                {
+
+                }
+            },
+        },
+        modern:
+        {
+
+        },
+        postmodern:
+        {
+
+        }
+    }
+}
